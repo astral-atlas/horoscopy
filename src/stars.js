18c691cc377d45ebec845e75bc0284521d4a3c8e
Luke Kaalim <lukekaalim@gmail.com>
2019-07-01 12:03:01 +1000

    Add more star signs

--- src/stars.js
+++ src/stars.js
@@ -2,7 +2,7 @@ export const spite = {
   name: 'The Spiteful Stars',
   image: './images/stars/spite.svg',
   qualities: 'Resilient, Enduring, Vengence',
-  stars: 'First Dawn\'s Kiss, Mooncrest, Loathsome Boggart, Unglich Fezzleton, Whoreson Prime',
+  stars: 'First Dawn\'s Kiss, Mooncrest, Loathsome Boggart, Unglich Fizzwhistler, Whoreson Prime',
   description: [
     'The myth goes: A pair of astronomers fall in love, and name the first stars they discover after each other.',
     'But one of the atronomers gaze wanders; he leaves for another',
@@ -13,24 +13,80 @@ export const spite = {
 export const grey = {
   name: 'Grey Space',
   image: './images/stars/grey-space.svg',
-  qualities: 'Ancient, Origins, Respect'
+  qualities: 'Ancient, Origins, Respect',
+  description: [
+    'Ancient an interposing, the stars that form the Grey Space are rumored to be amongst the eldest there are.',
+    'Some stories even mention the Grey Space as the inspiration for many spells, but how this is possible is unclear.'
+  ],
+  stars: 'Liga, Celene, Luna',
 };
 
 export const inevitable = {
   name: 'The Inevitable',
   image: './images/stars/inevitable.svg',
-  qualities: 'Knowlege, Time, Enduring'
+  qualities: 'Knowlege, Time, Enduring',
+  description: [
+    'Eternally and at an exact pace, Trun orbits Furth-Wuth.',
+    'Those that are born under this star sign are said to be unending in their determination and stamina.',
+    'Ancient Calendars used to track time using Trun\'s circle.',
+  ],
+  stars: 'Trun, Furth-Wuth',
 };
 
 export const transmuted = {
   name: 'The Transmuted Stars',
   image: './images/stars/transmuted.svg',
-  qualities: 'Changing, Flexible, Mysterious'
+  qualities: 'Changing, Flexible, Mysterious',
+  description: [
+    'The Legendary Myth of the Sorcerer who attempted to transmute a star is linked to this cluster.',
+    'The stories says the act of attempting the feat changed the sorcerer irrevocably.',
+    'But to what end: none say.'
+  ],
+  stars: 'Beleor, Milithurl, Yvvis',
 };
+
+export const flowstar = {
+  name: 'Flowstar',
+  qualities: 'Refreshing, Restoring, Healing',
+  image: './images/stars/flowstar.svg',
+  description: [
+    'Those born under the Flowstar are said to be imbibed with a calming energy.',
+    'Their very presence eases the soul of worries.',
+    'But this comes at a cost; they take others troubles into themselves, and pay for it.'
+  ],
+  stars: 'Flowstar',
+}
+
+export const unicorn = {
+  name: 'Unicorn',
+  qualities: 'Unique, Arcane, Fey',
+  image: './images/stars/unicorn.svg',
+  description: [
+    'The Unicorn is a rare star sign, due to its astrological position, very rarely is one born under it',
+    'Those that are born under it are fated to endure tragedy, according to elvish lore.'
+  ],
+  stars: 'Horn, Julias I, Julias II, Ulliander, Ethyrias',
+}
+
+export const hastur = {
+  name: 'Hastur',
+  qualities: 'Accursed, Doomed',
+  image: './images/stars/hastur.svg',
+  description: [
+    'Hastur marks doom.',
+    'Some elves exile those that they discover are born under Hastur',
+    'Hastur is said to be a terrible evil, or a benevolent but dead force for good',
+    'Neither interpretation bodes well for those that bear it.'
+  ],
+  stars: 'Hastur',
+}
 
 export const starSigns = [
   spite,
   inevitable,
   grey,
   transmuted,
+  flowstar,
+  unicorn,
+  hastur,
 ];
